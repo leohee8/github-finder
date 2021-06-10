62449d506864ce05afb5e66b4ffa0ab1585e5e71
Leo Hee Fook Yew <leohee@gmail.com>
2021-06-10 23:18:03 +0800

    Some changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React,{Component,Fragment} from "react"
 import "./App.css"
 
 class App extends Component{
+	getBirthday=()=>"8th August 1979"
 	render(){
+		const
+			myName="Leo Hee Fook Yew",
+			loading=false,
+			showName=true
+
 		return(
 			<Fragment>
 				<h1>Github Finder Project</h1>
+				{loading?<h4>Loading...</h4>:<p>Hi! Welcome {showName && myName.toUpperCase()}</p>}
+				<p>Your birthday is {this.getBirthday()}</p>
 			</Fragment>
 		)
 	}
